d1004d3f59dc32f38ded1f0c0a68bc1ce3970b6a
XIAOXIAODESHUTONG <wuyueyuedejia@gmail.com>
2020-04-26 23:09:25 +0200

    fix second error

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -36,7 +36,7 @@ public class TorpedoStore {
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-       throw new IllegalArgumentException("numberOfTorpedos");
+      throw new IllegalArgumentException("numberOfTorpedos");
     }
 
     boolean success = false;
@@ -47,7 +47,7 @@ public class TorpedoStore {
 
     if (r >= FAILURE_RATE) {
       // successful firing
-      this.torpedoCount = - numberOfTorpedos;
+      this.torpedoCount =- numberOfTorpedos;
       success = true;
     } else {
       // simulated failure
